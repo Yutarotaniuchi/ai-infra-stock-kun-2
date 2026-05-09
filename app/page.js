96f1040ad4655875ae773cfde5f9e445699adb0c
Yutarotaniuchi <oo.kyrios.003@gmail.com>
2026-05-09 09:35:49 +0900

    first app

---
+++ app/page.js
@@ -0,0 +1,16 @@
+export default function Page() {
+  return (
+    <main style={{
+      background: "#05080F",
+      color: "white",
+      minHeight: "100vh",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      fontSize: "32px",
+      fontWeight: "bold"
+    }}>
+      AI Infra Stock Kun
+    </main>
+  )
+}
